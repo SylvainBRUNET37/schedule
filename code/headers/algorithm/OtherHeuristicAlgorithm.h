dcaeea555ba948c4bff53417811b314ca9c64064
Sylvain <sylvain.brunet37@gmail.com>
2024-11-30 14:11:34 +0100

    refactor

--- code/headers/algorithm/OtherHeuristicAlgorithm.h
+++ code/headers/algorithm/OtherHeuristicAlgorithm.h
@@ -1,12 +1,11 @@
 #ifndef OTHER_HEURISTIC_AGLORITHM_H
 #define OTHER_HEURISTIC_AGLORITHM_H
 
-#include "ResolutionAlgorithm.h"
+#include "HeuristicAlgorithm.h"
 
-class OtherHeuristicAlgorithm : public ResolutionAlgorithm
+class OtherHeuristicAlgorithm : public HeuristicAlgorithm
 {
 private:
-
 	/*****************************************************
 	*                 GLOBAL VERIFICATION                *
 	*****************************************************/
@@ -22,7 +21,7 @@ private:
 	 * Verifies various conditions such as day off status, weekend availability, maximum worked time,
 	 * and maximum consecutive worked days to determine if the nurse is available.
 	 */
-	bool isAvailableThisDay(unsigned int nurseId, unsigned int dayId);
+	bool isAvailableThisDay(unsigned int nurseId, unsigned int dayId) override;
 
 	/**
 	 * @brief Checks if a nurse is available to work a specific shift on a given day.
@@ -35,7 +34,7 @@ private:
 	 *
 	 * Verifies the availability of the nurse based on the previous day's work and shift type limits.
 	 */
-	bool isAvailableForShift(unsigned int nurseId, unsigned int dayId, unsigned int shiftId);
+	bool isAvailableForShift(unsigned int nurseId, unsigned int dayId, unsigned int shiftId) override;
 
 public:
 	/*****************************************************
@@ -50,42 +49,10 @@ public:
 	 * Initializes the algorithm with the given instance,
 	 * fills the list of nurses, and fill the solution matrix with -1.
 	 */
-	OtherHeuristicAlgorithm(Instance& instance) : ResolutionAlgorithm(instance) {
+	OtherHeuristicAlgorithm(Instance& instance) : HeuristicAlgorithm(instance) {
 		// Initialize the solution: set every nurse's shift assignment for each day to -1 (meaning the nurse is not working that day
 		bestSolution.v_v_IdShift_Par_Personne_et_Jour.resize(instance.get_Nombre_Personne(), vector<int>(instance.get_Nombre_Jour(), -1));
 	}
-
-	/*****************************************************
-	*                  ALGORITHM PARTS                   *
-	*****************************************************/
-
-	/**
-	 * @brief Executes the heuristic algorithm to find the best scheduling solution.
-	 *
-	 * @return A pointer to the bestSolution object containing the scheduling results.
-	 */
-	Solution& run() override;
-
-	/**
-	 * @brief Allocates shifts for a specific day.
-	 *
-	 * @param dayId The ID of the day for which shifts are being allocated.
-	 *
-	 * Assigns nurses to shifts based on requirements for the day.
-	 * Continues allocating until all shifts are filled or no more nurses are available.
-	 */
-	void allocateDay(unsigned int dayId, vector<unsigned int>& availableNurses);
-
-	/**
-	 * @brief Allocates a specific shift to a nurse.
-	 *
-	 * @param nurseId The ID of the nurse to allocate.
-	 * @param dayId The ID of the day for the shift.
-	 * @param shiftId The ID of the shift to be allocated.
-	 *
-	 * @return True if the shift was allocated successfully; false otherwise.
-	 */
-	bool allocateShift(unsigned int nurseId, unsigned int dayId, unsigned int shiftId);
 };
 
 #endif
--- code/src/algorithm/MinHeuristicAlgorithm.cpp
+++ code/src/algorithm/MinHeuristicAlgorithm.cpp
@@ -4,7 +4,6 @@
 #include <functional>
 
 #include "../../headers/algorithm/MinHeuristicAlgorithm.h"
-#include "../../headers/calculation/CompleteObjectiveCalculator.h"
 
 /*****************************************************
 *                 GLOBAL VERIFICATION                *
@@ -39,9 +38,6 @@ bool MinHeuristicAlgorithm::isAvailableThisDay(unsigned int nurseId, unsigned in
 		}
 	}
 
-	// Fait travailler si le nombre de jours minimum de travail consécutif n'est pas atteint
-	//if (!validator.haveDoneMinConsecutiveWorkedDay(nurseId, dayId)) return true;
-
 	// Check if at max consecutive worked days
 	if (validator.isAtMaxConsecutiveWorkedDay(nurseId, dayId)) return false;  // Medium frequency
 
@@ -65,117 +61,17 @@ bool MinHeuristicAlgorithm::isAvailableForShift(unsigned int nurseId, unsigned i
 						bestSolution.v_v_IdShift_Par_Personne_et_Jour[nurseId][dayId - 1] = testShiftId;
 						--data.maxShiftsPerType[nurseId][testShiftId];
 						break;
-						// changer missing nurse si besoin
 					}
 				}
 			}
 		}
-	}
 
-	// Check succession forbidden
-	if (dayId != 0 && validator.isWorkingThisDay(nurseId, dayId - 1))
 		if (validator.isSuccessionForbidden(shiftId, bestSolution.v_v_IdShift_Par_Personne_et_Jour[nurseId][dayId - 1]))
 			return false;
+	}
 
 	// Check for the specific shift type's limits
 	if (validator.isAtMaxWorkedShift(nurseId, shiftId)) return false;	
 
-	// Check if the nurse has reached the max worked time
-	//if (validator.isAtMaxWorkedTime(nurseId, shiftId)) return false;
-
 	return true;
-}
-
-/*****************************************************
-*                  ALGORITHM PARTS                   *
-*****************************************************/
-
-Solution& MinHeuristicAlgorithm::run()
-{
-	unsigned int dayId = 0;
-	unsigned int nbDay = instance.get_Nombre_Jour();
-
-	random_device rd;
-	mt19937 eng(rd());
-
-	for (unsigned int dayId : data.days)
-	{
-		vector<unsigned int> availableNurses;
-
-		// Insert available nurses
-		for (unsigned int nurseId : data.nurses)
-			if (isAvailableThisDay(nurseId, dayId))
-				availableNurses.push_back(nurseId);
-
-		shuffle(availableNurses.begin(), availableNurses.end(), eng);
-
-		// Allocate every nurse
-		allocateDay(dayId, availableNurses);
-	}
-
-	CompleteObjectiveCalculator calculator;
-
-	bestSolution.i_valeur_fonction_objectif = calculator.calculateObjectiveFunction(instance, bestSolution);
-
-	return bestSolution;
-}
-
-void MinHeuristicAlgorithm::allocateDay(unsigned int dayId, vector<unsigned int>& availableNurses)
-{
-	random_device rd;
-	mt19937 eng(rd());
-	unsigned int nbShift = instance.get_Nombre_Shift();
-
-	for (auto nurseIterator = availableNurses.begin(); nurseIterator != availableNurses.end();)
-	{
-		unsigned int nurseId = *nurseIterator;
-		bool shifted = false;
-
-		shuffle(data.shifts.begin(), data.shifts.end(), eng);
-
-		for (unsigned int shiftId : data.shifts)
-		{
-			if (allocateShift(nurseId, dayId, shiftId))
-			{
-				// Affectation réussie, marquer comme décalée
-				shifted = true;
-				// Supprimer l'infirmière de l'ensemble
-				nurseIterator = availableNurses.erase(nurseIterator); // Supprimer et obtenir le nouvel itérateur
-				break; // Sortir de la boucle de shifts
-			}
-		}
-
-		if (!shifted)
-			++nurseIterator; // Passer à l'infirmière suivante si aucune affectation n'a eu lieu
-
-		if (availableNurses.empty())
-			return; // Sortir si plus d'infirmières disponibles
-	}
-}
-
-bool MinHeuristicAlgorithm::allocateShift(unsigned int nurseId, unsigned int dayId, unsigned int shiftId)
-{
-	// Check if the nurse is eligible to work on the specified day and shift
-	if (isAvailableForShift(nurseId, dayId, shiftId))
-	{
-		// Assign the shift to the nurse in the best solution
-		bestSolution.v_v_IdShift_Par_Personne_et_Jour[nurseId][dayId] = shiftId; // Allocate shift to the nurse
-		data.nbMinuteWorked[nurseId] += instance.get_Shift_Duree(shiftId); // Update the total minutes worked by the nurse
-
-		// Decrement the remaining shifts for this nurse on the current shift
-		if (data.maxShiftsPerType[nurseId][shiftId] > 0)
-			--data.maxShiftsPerType[nurseId][shiftId];
-
-		// Increment the number of weekend worked if it's a weekend day
-		if (validator.isWeekendDay(dayId))
-			data.nbWeekendWorked[nurseId] += 1;
-
-		// Decrement the numeber of nurse needed for the shift and the day
-		--data.missingNursePerShift[dayId][shiftId];
-		--data.missingNursePerDay[dayId];
-
-		return true; // Allocation was successful
-	}
-
-	return false; // Allocation failed due to eligibility
 }
