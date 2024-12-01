ede1862447cb0a859f42697f7e35e80099a0a4c5
Sylvain <sylvain.brunet37@gmail.com>
2024-12-01 16:10:25 +0100

    Removing usless calulator

--- code/src/calculation/LightObjectiveCalculator.cpp
+++
@@ -1,7 +0,0 @@
-#include "../../headers/calculation/LightObjectiveCalculator.h"
-
-int LightObjectiveCalculator::calculateObjectiveFunction(Instance& instance, Solution& solution)
-{
-	// Fonction objectif sans les pénalités sur les refus d'affectations et les refus de repos
-	return 0;
-}
