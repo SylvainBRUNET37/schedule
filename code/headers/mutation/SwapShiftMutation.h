0ebc40d6828d68689370d01b148522d415662c2f
Sylvain <sylvain.brunet37@gmail.com>
2024-12-01 16:42:54 +0100

    commentaires mutations

--- code/headers/mutation/SwapShiftMutation.h
+++ code/headers/mutation/SwapShiftMutation.h
@@ -3,14 +3,26 @@
 
 #include "MutationStrategy.h"
 
+/**
+ * @brief Implements a mutation strategy that swaps shifts within a nurse's schedule.
+ *
+ * The `SwapShiftMutation` class performs a mutation by randomly selecting a nurse's schedule
+ * and rotating shifts between two randomly chosen points to introduce variability.
+ */
 class SwapShiftMutation : public MutationStrategy
 {
 private:
 
 public:
-	SwapShiftMutation() {}
-
-	void execute(Solution& solution) override;
+    /**
+     * @brief Executes a swap shift mutation on a nurse's schedule.
+     *
+     * @param solution A reference to the current solution containing schedules for all nurses.
+     *
+     * This mutation modifies the schedule of a randomly selected nurse by rotating
+     * the shifts between two randomly chosen crossover points.
+     */
+    void execute(Solution& solution) override;
 };
 
 #endif
